043342ed938ba8db1bac5b1574e4ffcc926c9bc9
Ron Smits <ron.smits@gmail.com>
2014-10-08 22:35:17 +0200

    communiceer met de verticle

--- sender/test.js
+++ sender/test.js
@@ -1,27 +1,32 @@
 var Bleacon = require('bleacon');
 console.log("started");
-// Bleacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
 
-var ws = require('ws').Server, wss = new ws({port:8080});
+// Create a websocket server. This has to be done in node as
+// normal javascript cannot create a server only a listener
+var ws = require('ws');
+var socket = new ws("ws://192.168.0.106:9876");
 
-var connection;
-   wss.on('connection', function(connection) {
-      console.log('connected');
-   Bleacon.on('discover', function(bleacon) {
-      connection.send(JSON.stringify(bleacon));
-   });
-  connection.on('error', function(error) {console.log("ERR: " + error)});
+socket.on('open', function(){
+	console.log('opened');
+socket.send("testing");
+	
 });
 Bleacon.on('discover', function(bleacon) {
-//    console.log('found: ' + JSON.stringify(bleacon));
-    distance(bleacon);
+		var info = '{"position":"P1", "beacon":" '
+			+'"'+bleacon.major+'", '
+			+'"rssi":'+bleacon.rssi+', '
+			+'"distance:"'+distance(bleacon);
+			+'}';
+ 		socket.send(info);
 });
-//Bleacon.startScanning('e2c56db5dffb48d2b060d0f5a71096e0');
+
 Bleacon.startScanning('b9407f30f5f8466eaff925556b57fe6d');
-//Bleacon.startScanning('b9407f30f5f8466eaff925556b57fe6d', 21376);
-//Bleacon.startScanning();
+
+
+// calculate the distance. we dont really do a lot with this.
 var distance = function(bleacon) {
   var dist = Math.pow(10.0, ((bleacon.measuredPower+(-1*bleacon.rssi))/(10.0*2.2)));
-//  console.log(bleacon.uuid+" " +bleacon.major+" "+dist.toFixed(9));
-console.log(bleacon.uuid+" " + bleacon.major+" " + bleacon.rssi);
+  console.log(bleacon.uuid+" " +bleacon.major+" "+dist);
+return dist;
+  //console.log(bleacon.uuid+" " + bleacon.major+" " + bleacon.rssi);
 }
